501fa5305db14a4caeafa7c4a091749c1e0fed67
AlexiaGossa <102846702+AlexiaGossa@users.noreply.github.com>
2026-01-11 02:01:35 +0100

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,17 @@
+<?php
+
+$sFileKeyUID 				    = 	__DIR__ . '/logs/key_uid.txt';
+$sFileNameInputData 		= 	__DIR__ . '/logs/input_data.json';
+$sFileNameAuthData 			= 	__DIR__ . '/logs/auth_data.json';
+
+$redirectUri 			      = 	"https://twitch.nemelit.com/oauth.php";
+$authorizeUri			      =   "https://id.twitch.tv/oauth2/authorize";
+$tokenUri				        =	  "https://id.twitch.tv/oauth2/token";
+
+$iRefreshMargin			    =	  300; //5 minutes
+
+$scopes 				        = 	[
+								              "moderator:read:followers",
+								              "channel:read:subscriptions",
+								              "bits:read",
+							              ];
