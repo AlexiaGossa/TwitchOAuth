795d7fbf18190dee573d097ddc79773f425f16f3
AlexiaGossa <102846702+AlexiaGossa@users.noreply.github.com>
2026-01-11 02:06:48 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,14 +1,14 @@
 <?php
 
 $sFileKeyUID 				    = 	__DIR__ . '/logs/key_uid.txt';
-$sFileNameInputData 		= 	__DIR__ . '/logs/input_data.json';
-$sFileNameAuthData 			= 	__DIR__ . '/logs/auth_data.json';
+$sFileNameInputData 			= 	__DIR__ . '/logs/input_data.json';
+$sFileNameAuthData 				= 	__DIR__ . '/logs/auth_data.json';
 
-$redirectUri 			      = 	"https://twitch.nemelit.com/oauth.php";
-$authorizeUri			      =   "https://id.twitch.tv/oauth2/authorize";
+$redirectUri 			      	= 	"https://twitch.nemelit.com/oauth.php";
+$authorizeUri			      	=   "https://id.twitch.tv/oauth2/authorize";
 $tokenUri				        =	  "https://id.twitch.tv/oauth2/token";
 
-$iRefreshMargin			    =	  300; //5 minutes
+$iRefreshMargin			    	=	  300; //5 minutes
 
 $scopes 				        = 	[
 								              "moderator:read:followers",
